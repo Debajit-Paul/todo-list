de41a1ef90f71cada584d23378aa41593b4c5c7a
Debajit Paul <debajitfan.of@gmail.com>
2023-02-21 16:51:43 +0530

    update: style changes

--- script.js
+++ script.js
@@ -3,15 +3,20 @@ let todoBtn = document.querySelector("#todo-button");
 let todoDiv = document.querySelector(".box");
 let todoList = document.querySelector("#todo-list");
 
+// window.addEventListener("mousemove", e =>{
+
+// })
 
 todoBtn.addEventListener("click", e => {
     console.log(inputText.value);
     e.preventDefault();
     let listItem = document.createElement("li");
     let newDiv = document.createElement("div");
-    newDiv.innerText = inputText.value;
-    newDiv.className = "box"
-    listItem.appendChild(newDiv);
-    todoList.appendChild(listItem);
-    inputText.value = "";
+    if(inputText.value != ""){
+        newDiv.innerText = inputText.value;
+        newDiv.className = "box"
+        listItem.appendChild(newDiv);
+        todoList.appendChild(listItem);
+        inputText.value = "";
+    }
 })
