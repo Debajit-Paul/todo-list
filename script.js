5b3c4226ef1a066cf8bb3f3e0b9b88c36ae93139
Debajit Paul <debajitfan.of@gmail.com>
2023-02-21 21:40:29 +0530

    update: style

--- script.js
+++ script.js
@@ -8,15 +8,29 @@ let todoList = document.querySelector("#todo-list");
 // })
 
 todoBtn.addEventListener("click", e => {
-    console.log(inputText.value);
     e.preventDefault();
-    let listItem = document.createElement("li");
-    let newDiv = document.createElement("div");
     if(inputText.value != ""){
-        newDiv.innerText = inputText.value;
+        // create <div></div>
+        let newDiv = document.createElement("div");
         newDiv.className = "box"
+        // create <li></li>
+        let listItem = document.createElement("li");
+        listItem.classList.add("todo-item");
+        newDiv.innerText = inputText.value;
+        
+        inputText.value = "";
+        
         listItem.appendChild(newDiv);
         todoList.appendChild(listItem);
-        inputText.value = "";
+        // check mark button
+        const completeBtn = document.createElement("button");
+        completeBtn.innerHTML = ' <i class="fas fa-check"></i>';
+        completeBtn.classList.add("complete-btn");
+        listItem.appendChild(completeBtn);
+        // trash button
+        const trashBtn = document.createElement("button");
+        trashBtn.innerHTML = '<i class="fas fa-trash"></i>';
+        trashBtn.classList.add("trash-btn");
+        listItem.appendChild(trashBtn);
     }
 })
